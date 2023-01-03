20fd0c95c94cee4dd1a41288b0fb55e72a8f6175
Lucdomingues <lojalucc@outlook.com>
2023-01-03 14:27:06 -0300

    [requisito-03-completed]-feat

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,6 +5,15 @@ function Table() {
   const context = useContext(MyContext);
   const [apiResults, setApiResults] = useState([]);
   const [inputName, setInputName] = useState('');
+  const [genericFilter, setGenericFilter] = useState([]);
+  const [colummn] = useState([
+    'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
+  ]);
+  const [selected, setSelected] = useState({
+    column: 'population',
+    condition: 'maior que',
+    value: 0,
+  });
 
   useEffect(() => {
     async function responseApi() {
@@ -14,20 +23,87 @@ function Table() {
     responseApi();
   }, [context.apiResponse]);
 
-  const resultSearch = apiResults
+  const resultSearchName = apiResults
     .filter((element) => element.name.includes(inputName));
+
+  const resultsFilterNumber = () => {
+    const { column, condition, value } = selected;
+    const filterDad = resultSearchName.filter((element) => {
+      switch (condition) {
+      case 'maior que':
+        return Number(element[column]) > Number(value);
+      case 'igual a':
+        return Number(value) === Number(element[column]);
+      case 'menor que':
+        return Number(element[column]) < Number(value);
+      default:
+        return true;
+      }
+    });
+    return filterDad;
+  };
+
+  const isFiltered = genericFilter.length > 0 ? genericFilter : resultSearchName;
+
+  console.log(isFiltered);
 
   return (
     <div>
       <form>
         <input
           type="text"
-          name="filter-name"
-          id="filter-name"
+          name="name"
+          id="name-filtered"
           data-testid="name-filter"
           placeholder="buscar"
+          value={ inputName }
           onChange={ ({ target }) => setInputName(target.value) }
         />
+        <select
+          name="column"
+          id="column-filtered"
+          data-testid="column-filter"
+          value={ selected.column }
+          onChange={ ({ target }) => setSelected({
+            ...selected, column: target.value,
+          }) }
+        >
+          {colummn.map((element) => (
+            <option key={ element } value={ element }>
+              {element}
+            </option>
+          ))}
+        </select>
+        <select
+          name="comparison"
+          id="comparison-filtered"
+          data-testid="comparison-filter"
+          value={ selected.condition }
+          onChange={ ({ target }) => setSelected({
+            ...selected, condition: target.value,
+          }) }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          type="number"
+          name="value"
+          id="value-filtered"
+          data-testid="value-filter"
+          value={ selected.value }
+          onChange={ ({ target }) => setSelected({
+            ...selected, value: target.value,
+          }) }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ () => setGenericFilter(resultsFilterNumber()) }
+        >
+          FILTRAR
+        </button>
       </form>
       <table>
         <thead>
@@ -48,7 +124,7 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {resultSearch.map((api) => (
+          {isFiltered.map((api) => (
             <tr key={ Math.random() }>
               <td>
                 {api.name}
