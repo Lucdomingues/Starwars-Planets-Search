e310fa6a2f3c0ae48d6eb3ae430e5419e430ab38
Lucdomingues <lojalucc@outlook.com>
2023-01-12 19:39:30 -0300

    [test]on-branches

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -16,7 +16,7 @@ afterEach(() => {
   jest.resetAllMocks();
 });
 
-it('Verifica filter number', async () => {
+it('Verifica filter number igual a', async () => {
   render(<App />);
 
   const column = screen.getByTestId('column-filter');
@@ -30,6 +30,70 @@ it('Verifica filter number', async () => {
   const button = screen.getByTestId('button-filter')
   userEvent.click(button);
 
-  const tatooine = await screen.getByRole('cell', { name: /tatooine/i });
-  expect(tatooine).toBeInTheDocument()
+  const tattoine = await screen.findByText('Tatooine')
+  expect(tattoine).toBeInTheDocument()
+})
+
+it('Verifica a funcionalidade remove', () => {
+  render(<App />);
+
+  const buttonFinter = screen.getByRole('button', { name: /filtrar/i })
+
+  expect(buttonFinter).toBeInTheDocument();
+  userEvent.click(buttonFinter)
+
+  const buttonRemove = screen.getByRole('button', { name: /x/i })
+
+  expect(buttonRemove).toBeInTheDocument();
+  userEvent.click(buttonRemove)
+
+  expect(buttonRemove).not.toBeInTheDocument()
+})
+it('Verifica a funcionalidade remove all', () => {
+  render(<App />);
+
+  const buttonFinter = screen.getByRole('button', { name: /filtrar/i })
+  const buttonRemoveAll = screen.getByRole('button', { name: /remover filtros/i })
+
+  expect(buttonFinter).toBeInTheDocument();
+  userEvent.click(buttonFinter)
+
+  const buttonRemove = screen.getByRole('button', { name: /x/i })
+
+  expect(buttonRemove).toBeInTheDocument();
+  userEvent.click(buttonRemoveAll)
+
+  expect(buttonRemove).not.toBeInTheDocument()
+})
+
+it('Verifica se ao digitar no input faz a renderização certa', async () => {
+  render(<App />);
+
+  const inputName = screen.getByRole('textbox')
+
+  expect(inputName).toBeInTheDocument();
+
+  userEvent.type(inputName, 'Ta')
+
+  const tattoine = await screen.findByText('Tatooine')
+
+  expect(tattoine).toBeInTheDocument();
+})
+
+it('Verifica filter number menor que', async () => {
+  render(<App />);
+
+  const column = screen.getByTestId('column-filter');
+  const condition = screen.getByTestId('comparison-filter')
+  const inputNumber = screen.getByRole('spinbutton')
+
+  userEvent.selectOptions(column, ['population'])
+  userEvent.selectOptions(condition, ['menor que'])
+  userEvent.type(inputNumber, '2000');
+
+  const button = screen.getByTestId('button-filter')
+  userEvent.click(button);
+
+  const yavin = await screen.findByText('Yavin IV')
+  expect(yavin).toBeInTheDocument()
 })
